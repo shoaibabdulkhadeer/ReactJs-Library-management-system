2fa56d9f287350473cad4825b6adb3ccd065b78f
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-04-03 11:13:07 +0530

    added links to our homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className='Home'>
         <h1>Login As...?</h1>
+       <Link to="/admin"> Admin Login</Link>
+       <Link to="/userlogin"> User Login </Link>
 
 
     </div>

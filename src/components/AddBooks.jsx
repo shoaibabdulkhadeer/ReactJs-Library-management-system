dc9fdf44d7fa46b3ff86cc372e85ae17a97caf5d
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-04-04 15:11:03 +0530

    Navbar minor refractory

--- src/components/AddBooks.jsx
+++ src/components/AddBooks.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const AddBooks = () => {
-  return (
-    <div>
-        <h1>Add Books</h1>
+    return (
+        <div>
+            <h1>Add Books</h1>
         </div>
-  )
+    )
 }
 
 export default AddBooks
